33e414db5670659212c8f1169e0e758839925e1c
Kent Waxman <kawaxman@icloud.com>
2022-04-14 10:51:58 -0600

    Update stream.py

--- PUSHING_PELOSI/src/stream.py
+++ PUSHING_PELOSI/src/stream.py
@@ -21,7 +21,7 @@ def grab_trades_to_make():
         'X-CSRFToken': cf.CSRFTOKEN,
         'Authorization': 'Token ' + cf.QQ_API_TOKEN
         }
-        print(cf.CSRFTOKEN, cf.QQ_API_TOKEN, cf.QQ_ENDPOINT)
+        print("CSRF", cf.CSRFTOKEN, "api", cf.QQ_API_TOKEN,"endpoint", cf.QQ_ENDPOINT)
         req = requests.get(cf.QQ_ENDPOINT, headers=headers)
         req.raise_for_status()
 
