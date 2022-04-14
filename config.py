8a895f0f4020b13f77e66d29d088e336c538bcd2
Kent Waxman <kawaxman@icloud.com>
2022-04-13 18:59:13 -0600

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-#API_KEY - The API key for the Alpaca Trade API
-API_KEY = os.environ['API_KEY']
-
-#API_SECRET - The Secret API key for the Alpaca Trade API
-API_SECRET = os.environ['API_SECRET']
-
-#ALPACA_ENDPOINT - The url to draw data from
-ALPACA_ENDPOINT = 'https://paper-api.alpaca.markets'
